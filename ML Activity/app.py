5849793399d020c92656747f12049afd5b51c16a
EliteSoda <alarconrusselnathan0512@gmail.com>
2026-03-30 04:38:45 +0800

    update app.py

--- ML Activity/app.py
+++ ML Activity/app.py
@@ -6,7 +6,7 @@ from PIL import Image
 # Load the model
 @st.cache_resource
 def load_model():
-    return tf.keras.models.load_model("ML Activity/converted_keras/keras_model.h5")
+    return tf.keras.models.load_model("ML Activity/converted_savedmodel")
 
 model = load_model()
 
